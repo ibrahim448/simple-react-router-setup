1e2ffdff673a192a214f87d5f8d9d621ae2e55ba
Ibrahim <mdibrahim.bd320@gmail.com>
2023-05-09 23:49:20 +0600

    first commit

---
+++ _setup-router.js
@@ -0,0 +1,8 @@
+/*
+
+1.   install router   :   npm i react-router-dom
+2.   create a router use createBrowserRouter
+3.   add router provider and pass router props
+4.   create some route insidethe router
+
+*/
